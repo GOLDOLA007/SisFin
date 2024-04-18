5fb4655923843a8634fbc45c5fefaeb50a6f2b76
Kauã Oliveira <105607462+GOLDOLA007@users.noreply.github.com>
2024-04-18 11:08:55 -0300

    Add files via upload

---
+++ MATIOLI/Dados/Compra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class Compra
+    {
+        public int id { get; set; }
+        public DateTime instante { get; set; }
+        public String descricao { get; set; }
+        public Double desconto { get; set; }
+        public Double valor_total { get; set; }
+    }
+}
---
+++ MATIOLI/Dados/TipoConta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class TipoConta
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int tipo { get; set; }
+        public enum TipoCont {CONTA_A_PAGAR = 1, CONTA_A_RECEBER = 2};
+    }
+}
