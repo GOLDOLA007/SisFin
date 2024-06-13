f698a72fb985d03bfb27504923fce3e74a823f7d
Kauã Oliveira <105607462+GOLDOLA007@users.noreply.github.com>
2024-06-13 10:43:45 -0300

    Add files via upload

---
+++ SisFin-main/SisFin-main/MATIOLI/Dados/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class Conta
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+        public TipoConta tipo { get; set; }
+        public bool status { get; set; }
+    }
+}
---
+++ SisFin-main/SisFin-main/MATIOLI/Dados/MeioPagamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class MeioPagamento
+    {
+        public enum Pagamento {PIX = 1,  BOLETO = 2, CARTAO_DE_CREDITO = 3, CARTAO_DE_DEBITO = 4, DINHEIRO = 5  };
+    }
+}
